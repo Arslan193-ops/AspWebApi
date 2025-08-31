b9dbf7ba01b9ea761146aea0ec2e941c28682ab6
Arslan <arslanmian604@gmail.com>
2025-08-31 17:53:04 +0500

    created first api

---
+++ AspWebApi/Controllers/SportsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SportsController : ControllerBase
+    {
+        public List<string> sports = new List<string>
+        {
+            "Soccer",
+            "Basketball",
+            "Baseball",
+            "Tennis",
+            "Cricket"
+        };
+
+        [HttpGet]
+        public List<string> GetSports()
+        {
+            return sports;
+        }
+
+        [HttpGet("{Id}")]
+        public string GetSportsbyId(int Id)
+        {
+            return sports.ElementAt(Id);
+        }
+    }
+}
